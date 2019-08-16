f16fa37bd5cdfd84d826bf044fc8436d898dedcf
Masum <masums@gmail.com>
2019-08-16 18:19:03 +0600

    test completed.

--- electron.js
+++ electron.js
@@ -1,9 +1,12 @@
+//This is test electron app
 const {app, BrowserWindow, ipcMain} = require('electron');
 const {autoUpdater} = require("electron-updater");
 let win; // this wills store the window object
 
 function createDefaultWindow() {
-    win = new BrowserWindow({width: 900, height: 680});
+    win = new BrowserWindow({width: 900, height: 680, webPreferences: {
+      nodeIntegration: true,
+    }});
     win.loadURL(`file://${__dirname}/index.html`);
     win.on('closed', () => app.quit());
   return win;
@@ -11,10 +14,12 @@ function createDefaultWindow() {
 
 // when the update is ready, notify the BrowserWindow
 autoUpdater.on('update-downloaded', (info) => {
-    win.webContents.send('updateReady')
+  console.log("update found");  
+  win.webContents.send('updateReady')
 });
 app.on('ready', function() {
   createDefaultWindow();
+  console.log("checking update");
   autoUpdater.checkForUpdates();
 });
 ipcMain.on("quitAndInstall", (event, arg) => {
